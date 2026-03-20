0f11c1c49f9ee431991027918ffa276581d9d2bb
Taquitoo3000 <taquitodebistek23@gmail.com>
2026-03-20 11:23:31 -0600

    modificar link conexiones

--- components/header.js
+++ components/header.js
@@ -10,7 +10,7 @@ function loadHeader() {
                         <a href="https://taquitoo3000.pythonanywhere.com/">General</a>
                         <a href="subs.html">Subprocuradurías</a>
                         <a href="censo_trabajo.html">Desglose</a>
-                        <a href="http://127.0.0.1:8051/">Conexiones</a>
+                        <a href="http://172.17.3.34:8051/">Conexiones</a>
                     </div>
                 </div>
                 <a href="http://172.17.3.34:8501" class="boton_menu">SUNIED</a>
