e19e749a0757e564bb7358e6667ee12c6df3c3ed
Taquitoo3000 <taquitodebistek23@gmail.com>
2026-08-18 11:04:41 -0600

    corregir link sunied

--- components/header.js
+++ components/header.js
@@ -10,11 +10,10 @@ function loadHeader() {
                         <a href="https://taquitoo3000.pythonanywhere.com/">General</a>
                         <a href="subs.html">Subprocuradurías</a>
                         <a href="censo_trabajo.html">Desglose</a>
-                        <a href="http://172.17.2.25:8051/">Conexiones</a>
                     </div>
                 </div>
                 <a href="predicciones.html" class="boton_menu">Predicciones</a>
-                <a href="http://172.17.3.34:8501" class="boton_menu">SUNIED</a>
+                <a href="http://172.17.2.25:8501" class="boton_menu">SUNIED</a>
                 <a href="miembros.html" class="boton_menu">Miembros</a>
             </nav>
         </header>
