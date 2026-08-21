affde883186a1a0a2027db0e71c28d121e170ee9
Taquitoo3000 <taquitodebistek23@gmail.com>
2026-08-21 09:47:43 -0600

    cambiar SUNIED otra vez

--- components/header.js
+++ components/header.js
@@ -13,7 +13,7 @@ function loadHeader() {
                     </div>
                 </div>
                 <a href="predicciones.html" class="boton_menu">Predicciones</a>
-                <a href="http://172.17.2.25:8501" class="boton_menu">SUNIED</a>
+                <a href="http://172.17.2.54:8501" class="boton_menu">SUNIED</a>
                 <a href="miembros.html" class="boton_menu">Miembros</a>
             </nav>
         </header>
